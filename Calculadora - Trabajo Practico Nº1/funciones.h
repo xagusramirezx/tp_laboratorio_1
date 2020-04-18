3bbf297d1357484d75fbbad652272657addad8d2
xagusramirezx <aagusramirez@gmail.com>
2020-04-17 23:21:09 -0300

    trabajo final

---
+++ Calculadora - Trabajo Practico Nº1/funciones.h
@@ -0,0 +1,19 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void bienvenida();
+void menuCalculadora();
+
+float A;
+float B;
+
+float suma(float A,float B);
+float resta(float A,float B);
+float division(float A,float B);
+float multiplicacion(float A,float B);
+long long int factorial(float A);
+
+
+
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ Calculadora - Trabajo Practico Nº1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "funciones.h"
+
+
+int main( )
+{
+    bienvenida();
+    menuCalculadora();
+    return 0;
+}
+
